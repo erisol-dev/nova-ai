c364bf8aac59d5b9925ae348e2f25bdb5f900b61
Samuel Adesuyi <sadesuyi@gmu.edu>
2025-01-10 02:28:10 -0500

    Fixed project structure and added python backend and front end, turbopack fails here

---
+++ nova-ai/nova-ai-frontend/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
